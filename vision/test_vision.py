2c05921396349073041ecc4759315e72afc6cf28
yugwangsik <86756948+yugwangsik@users.noreply.github.com>
2021-07-20 03:56:00 +0100

    Add files via upload

---
+++ vision/test_vision.py
@@ -0,0 +1,35 @@
+def run_quickstart():
+    # [START vision_quickstart]
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    # [START vision_python_migration_import]
+    from google.cloud import vision
+    # [END vision_python_migration_import]
+
+    # Instantiates a client
+    # [START vision_python_migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END vision_python_migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath('vision/num.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.text_detection(image=image)
+    labels = response.text_annotations
+
+    for label in labels:
+        print(label.description)
+    # [END vision_quickstart]
+
+
+if __name__ == '__main__':
+    run_quickstart()
